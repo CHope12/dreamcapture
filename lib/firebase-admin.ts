5fca7e6c89599b3ecc4b77af809457fbcad9e5d3
jp010263 <87950396+CHope12@users.noreply.github.com>
2025-03-02 01:36:08 +0000

    admin config

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,7 +1,5 @@
 import admin from 'firebase-admin';
 
-const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n');
-
 export function initAdmin() {  
   if (!admin.apps.length) {
     
@@ -9,7 +7,7 @@ export function initAdmin() {
       credential: admin.credential.cert({
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: privateKey,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
       }),
       storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
     });
